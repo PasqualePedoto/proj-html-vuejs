e1b566669864aaad766e15f40901a3d91e46de0c
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-08-23 12:39:13 +0200

    Aggiunto un nuovo componente: BaseJumbotron.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <BaseHeader :headerLinks="headerLinks" />
+  <div>
+    <BaseHeader :headerLinks="headerLinks" />
+    <BaseJumbotron />
+  </div>
 </template>
 
 <script>
 import BaseHeader from "./components/BaseHeader.vue";
+import BaseJumbotron from "./components/BaseJumbotron.vue";
 export default {
   name: "App",
   components: {
     BaseHeader,
+    BaseJumbotron,
   },
   data() {
     return {
---
+++ src/components/BaseJumbotron.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="jumbotron">
+    <div class="jumbo">
+      <div class="container">
+        <div class="row">
+          <div class="col"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "BaseJumbotron",
+};
+</script>
+
+<style lang="scss" scoped>
+.jumbo {
+  background-image: url("../assets/images/home-3-hero-bg.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  width: 100%;
+  height: 700px;
+}
+</style>
