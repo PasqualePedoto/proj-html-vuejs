d32a16b6ee9fc4b8e6c2ed00aaa3071ab6613456
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-08-23 10:57:26 +0200

    Aggiunta un nuova dipendenza: Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
-
+  <div>ciao</div>
 </template>
 
-
-
+<script>
 export default {
-  name: 'App',
-  components: {
-
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
